d25a9817b3905ec4c064fdd0c61633e005db4e5c
Venkatesh Raju <venkatesh.raju20@gmail.com>
2025-04-19 15:19:47 +0530

    Created Project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {" "}
+      <h1 className="text-3xl font-bold underline text-avo-400">
+        Hello world!
+      </h1>
+      <div className="flex flex-col">
+        <div>test1</div>
+        <div>test2</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
